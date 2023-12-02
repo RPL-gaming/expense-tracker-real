ce03e303d5395448323f45078e9c017a317f628c
arkan.alexei <arkan.alexei@ui.ac.id>
2023-12-02 18:02:15 +0700

    refactor: prettier write

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -8,7 +8,7 @@ import { PromptTemplate } from "langchain/prompts";
 export const runtime = "edge";
 
 const formatMessage = (message: VercelChatMessage) => {
-    return `${message.role}: ${message.content}`;
+  return `${message.role}: ${message.content}`;
 };
 
 const TEMPLATE = `You are a sophisticated virtual financial advisor named Wiza. Your responses must be professional, informative, and tailored to individual financial queries. You provide advice on a range of topics including savings, investments, budgeting, and retirement planning, while maintaining a tone that is both approachable and authoritative
@@ -26,25 +26,25 @@ AI:`;
  * https://js.langchain.com/docs/guides/expression_language/cookbook#prompttemplate--llm--outputparser
  */
 export async function POST(req: NextRequest) {
-    try {
-        const body = await req.json();
-        const messages = body.messages ?? [];
-        const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
-        const currentMessageContent = messages[messages.length - 1].content;
-        const prompt = PromptTemplate.fromTemplate(TEMPLATE);
-        const model = new ChatOpenAI({
-            temperature: 0.8,
-        });
-        const outputParser = new BytesOutputParser();
-        const chain = prompt.pipe(model).pipe(outputParser);
+  try {
+    const body = await req.json();
+    const messages = body.messages ?? [];
+    const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
+    const currentMessageContent = messages[messages.length - 1].content;
+    const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+    const model = new ChatOpenAI({
+      temperature: 0.8,
+    });
+    const outputParser = new BytesOutputParser();
+    const chain = prompt.pipe(model).pipe(outputParser);
 
-        const stream = await chain.stream({
-            chat_history: formattedPreviousMessages.join("\n"),
-            input: currentMessageContent,
-        });
+    const stream = await chain.stream({
+      chat_history: formattedPreviousMessages.join("\n"),
+      input: currentMessageContent,
+    });
 
-        return new StreamingTextResponse(stream);
-    } catch (e: any) {
-        return NextResponse.json({ error: e.message }, { status: 500 });
-    }
+    return new StreamingTextResponse(stream);
+  } catch (e: any) {
+    return NextResponse.json({ error: e.message }, { status: 500 });
+  }
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script
-            type="text/javascript"
-            src="https://app.sandbox.midtrans.com/snap/snap.js"
-            data-client-key={process.env.MIDTRANS_CLIENT_KEY}
+          type="text/javascript"
+          src="https://app.sandbox.midtrans.com/snap/snap.js"
+          data-client-key={process.env.MIDTRANS_CLIENT_KEY}
         ></script>
       </head>
       <body className={inter.className}>{children}</body>
