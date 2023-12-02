cdf8681d5b6c5d2aa9d806376f76628d972d9092
arkan.alexei <arkan.alexei@ui.ac.id>
2023-12-02 16:47:00 +0700

    refactor: add prettier and linter for clean code

--- src/app/api/expenses/expense-service.ts
+++ src/app/api/expenses/expense-service.ts
@@ -3,47 +3,56 @@ import { isTokenValid } from "../../../../utils/auth/is-token-valid";
 import prisma from "../../../../lib/prisma";
 
 class ExpenseService {
-    async addExpense(req: NextRequest) {
-        if (req.headers.get('content-type') !== 'application/json') {
-            return NextResponse.json({ error: 'Invalid content-type. Must be application/json' }, { status: 400 });
-        }
-
-        const token = req.cookies.get('token')?.value;
-        let decoded;
-        if (token) {
-            decoded = isTokenValid(token);
-            if (!decoded) {
-                return NextResponse.json({ error: 'Invalid token' }, { status: 403 });
-            }
-        } else {
-            return NextResponse.json({ error: 'Invalid token' }, { status: 403 });
-        }
-
-        const { amount, date, category, description } = await req.json();
-        const userId = decoded.id;
-
-        // Validate data before saving
-        if (!amount || !date || !category || !userId) {
-            return NextResponse.json({ error: 'Incomplete data provided' }, { status: 400 });
-        }
-
-        try {
-            const newExpense = await prisma.expense.create({
-                data: {
-                    amount: parseFloat(amount),
-                    date: new Date(date),
-                    category,
-                    description,
-                    userId: userId,
-                },
-            });
-
-            return NextResponse.json(newExpense);
-        } catch (error) {
-            console.error(error);
-            return NextResponse.json({ error: 'An error occurred while adding the expense' }, { status: 500 });
-        }
+  async addExpense(req: NextRequest) {
+    if (req.headers.get("content-type") !== "application/json") {
+      return NextResponse.json(
+        { error: "Invalid content-type. Must be application/json" },
+        { status: 400 },
+      );
     }
+
+    const token = req.cookies.get("token")?.value;
+    let decoded;
+    if (token) {
+      decoded = isTokenValid(token);
+      if (!decoded) {
+        return NextResponse.json({ error: "Invalid token" }, { status: 403 });
+      }
+    } else {
+      return NextResponse.json({ error: "Invalid token" }, { status: 403 });
+    }
+
+    const { amount, date, category, description } = await req.json();
+    const userId = decoded.id;
+
+    // Validate data before saving
+    if (!amount || !date || !category || !userId) {
+      return NextResponse.json(
+        { error: "Incomplete data provided" },
+        { status: 400 },
+      );
+    }
+
+    try {
+      const newExpense = await prisma.expense.create({
+        data: {
+          amount: parseFloat(amount),
+          date: new Date(date),
+          category,
+          description,
+          userId: userId,
+        },
+      });
+
+      return NextResponse.json(newExpense);
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json(
+        { error: "An error occurred while adding the expense" },
+        { status: 500 },
+      );
+    }
+  }
 }
 
 export default ExpenseService;
