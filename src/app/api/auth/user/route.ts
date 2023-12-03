54fa2427036116c8e4aba0f50769dbd472f303db
mhadziqrazin <mhadziq0403@gmail.com>
2023-12-03 17:46:04 +0700

    feat: add utility to check is current user an advisor

---
+++ src/app/api/auth/user/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { isTokenValid } from '../../../../../utils/auth/is-token-valid';
+
+export async function GET(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+    let decoded;
+    if (token) {
+      decoded = isTokenValid(token);
+      if (!decoded) {
+        return NextResponse.json({ error: "Invalid token" }, { status: 403 });
+      }
+    } else {
+      return NextResponse.json({ error: "Invalid token" }, { status: 403 });
+    }
+
+    const userId = decoded.id;
+    const isAdvisor = decoded.isAdvisor;
+
+    return NextResponse.json({
+      userId,
+      isAdvisor
+    }, { status: 200 })
+}
